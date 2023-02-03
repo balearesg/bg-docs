06e4a6c169ada6ba26c8f2b2746ddd0e978c7198
Francisco Sanvicente Pico <64644109+FranciscoSanvicente@users.noreply.github.com>
2023-02-03 11:49:51 -0400

    ajuste

--- project/modules/content/index.ts
+++ project/modules/content/index.ts
@@ -1,10 +1,12 @@
 // IMPORTS
+import { Title } from '@bg/docs/components/html';
+
 import { default as __notFound } from './mdx/not-found.mdx';
 import { default as __compilacionQch } from './mdx/compilacion-qch/compilacion-qch.mdx';
 import { default as __plugins } from './mdx/plugins/plugins.mdx';
 import { default as __howToUse } from './mdx/how-to-use/how-to-use.mdx';
 import { default as __components } from './mdx/components/components.mdx';
-import { Title } from '@bg/docs/components/html'
+
 // EXPORTS
 export /*bundle*/ const NotFound = __notFound;
 export /*bundle*/ const CompilacionQch = __compilacionQch;
