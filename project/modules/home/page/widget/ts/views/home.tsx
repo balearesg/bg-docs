7713267068b66871a538a6ea2616f89f1878692b
Julio Rodriguez <jircdev@gmail.com>
2023-02-13 10:54:53 -0300

    fix: Add correct link to standard process

--- project/modules/home/page/widget/ts/views/home.tsx
+++ project/modules/home/page/widget/ts/views/home.tsx
@@ -1,40 +1,43 @@
-import * as React from 'react';
-import { Card } from './card';
+import * as React from "react";
+import { Card } from "./card";
 export function Home() {
-	return (
-		<div className="home">
-			<h1>Bienvenidos a la documentación Oficial de Baleares Group</h1>
+    return (
+        <div className="home">
+            <h1>Bienvenidos a la documentación Oficial de Baleares Group</h1>
 
-			<p>
-				En esta documentacion vas a poder encontrar informacion sobre los procesos
-				desarrollados en Baleares Group, asi como la documentacion de los proyectos que
-				usamos en el dia a dia a la hora de hacer aplicaciones de calidad
-			</p>
+            <p>
+                En esta documentacion vas a poder encontrar informacion sobre los procesos desarrollados en Baleares
+                Group, asi como la documentacion de los proyectos que usamos en el dia a dia a la hora de hacer
+                aplicaciones de calidad
+            </p>
 
-			<div className="card__container">
-				<Card title="Cómo públicar" href="/how-to-use">
-					Revisa la información de cómo agregar contenido a la documentación
-				</Card>
-				<Card title="Proceso de desarrollo" href="/how-to-use">
-					Estandar de desarrollo definido para el equipo de la factory.
-				</Card>
-				<Card title="BG Auth" href="/auth-instalation">
-					Pasos para la implementacion de bg-auth en aplicaciones.
-				</Card>
-				<Card title="BG Auth" href="/auth-instalation">
-					Documentacion para la integracion del objeto session del bg-auth en
-					aplicaciones.
-				</Card>
-				<Card title="BG Auth" href="/auth-instalation">
-					Documentacion del objeto para usuarios que provee bg-auth
-				</Card>
-				<Card title="Beyond UI" href="/how-to-use">
-					Implementacion de Beyond UI para la construccion de interfaces.
-				</Card>
-				<Card title="Essential IU" href="/how-to-use">
-					Implementacion de Essential-UI para la construccion de interfaces.
-				</Card>
-			</div>
-		</div>
-	);
+            <div className="card__container">
+                <Card title="Cómo públicar" href="/how-to-use">
+                    Revisa la información de cómo agregar contenido a la documentación
+                </Card>
+                <Card title="Proceso de desarrollo" href="/development/standards">
+                    Estandar de desarrollo definido para el equipo de la factory.
+                </Card>
+                <Card title="BG Auth" href="/auth-instalation">
+                    Pasos para la implementacion de bg-auth en aplicaciones.
+                </Card>
+                <Card title="BG Auth" href="/auth-instalation">
+                    Documentacion para la integracion del objeto session del bg-auth en aplicaciones.
+                </Card>
+                <Card title="BG Auth" href="/auth-instalation">
+                    Documentacion del objeto para usuarios que provee bg-auth
+                </Card>
+                <Card title="Beyond UI" href="/how-to-use">
+                    Implementacion de Beyond UI para la construccion de interfaces.
+                </Card>
+                <Card title="Essential IU" href="/how-to-use">
+                    Implementacion de Essential-UI para la construccion de interfaces.
+                </Card>
+
+                <Card title="Entrenamiento" href="/trainee">
+                    Implementacion de Essential-UI para la construccion de interfaces.
+                </Card>
+            </div>
+        </div>
+    );
 }
