1c5c2495851cb1fa20df12aaeb7b3182b8d93b22
Gabriel Castro <gaboj0531@gmail.com>
2023-04-13 10:20:32 -0400

    add:guide to configure git credentials

--- project/modules/content/index.ts
+++ project/modules/content/index.ts
@@ -1,23 +1,24 @@
 // IMPORTS
-import { Title, Info, Link } from '@bg/docs/components/html';
-import { Code } from '@bg/docs/code';
+import { Title, Info, Link } from "@bg/docs/components/html";
+import { Code } from "@bg/docs/code";
 
-import { default as __notFound } from './mdx/not-found.mdx';
-import { default as __compilacionQch } from './mdx/compilacion-qch/compilacion-qch.mdx';
-import { default as __plugins } from './mdx/plugins/plugins.mdx';
-import { default as __howToUse } from './mdx/how-to-use/how-to-use.mdx';
-import { default as __components } from './mdx/components/components.mdx';
-import { default as __developmentStandards } from './mdx/standards/development-process.mdx';
-import { default as _authInstalation } from './mdx/auth/auth-instalation.mdx';
-import { default as _authSession } from './mdx/auth/auth-session.mdx';
-import { default as _authUser } from './mdx/auth/auth-user.mdx';
-import { default as __essentialUI } from './mdx/ui-libraries/essential-ui.mdx';
-import { default as __beyondUI } from './mdx/ui-libraries/beyond-ui.mdx';
-import { default as __standars } from './mdx/standards/development-process.mdx';
-import { default as __mailer } from './mdx/mailer/mailer.mdx';
-import { default as __jview } from './mdx/jview/jview.mdx';
-import { default as __compilationBackend } from './mdx/compilation-backend-external-packages/compilation-backend-external-packages.mdx';
-import { default as __compilationMobile } from './mdx/mobile-compilations/flujocompilacionmobile.mdx';
+import { default as __notFound } from "./mdx/not-found.mdx";
+import { default as __compilacionQch } from "./mdx/compilacion-qch/compilacion-qch.mdx";
+import { default as __plugins } from "./mdx/plugins/plugins.mdx";
+import { default as __howToUse } from "./mdx/how-to-use/how-to-use.mdx";
+import { default as __components } from "./mdx/components/components.mdx";
+import { default as __developmentStandards } from "./mdx/standards/development-process.mdx";
+import { default as _authInstalation } from "./mdx/auth/auth-instalation.mdx";
+import { default as _authSession } from "./mdx/auth/auth-session.mdx";
+import { default as _authUser } from "./mdx/auth/auth-user.mdx";
+import { default as __essentialUI } from "./mdx/ui-libraries/essential-ui.mdx";
+import { default as __beyondUI } from "./mdx/ui-libraries/beyond-ui.mdx";
+import { default as __standars } from "./mdx/standards/development-process.mdx";
+import { default as __mailer } from "./mdx/mailer/mailer.mdx";
+import { default as __jview } from "./mdx/jview/jview.mdx";
+import { default as __compilationBackend } from "./mdx/compilation-backend-external-packages/compilation-backend-external-packages.mdx";
+import { default as __compilationMobile } from "./mdx/mobile-compilations/flujocompilacionmobile.mdx";
+import { default as __configureGit } from "./mdx/configure-git/configure-git.mdx";
 
 // EXPORTS
 export /*bundle*/ const NotFound = __notFound;
@@ -36,3 +37,4 @@ export /*bundle*/ const Mailer = __mailer;
 export /*bundle*/ const Jview = __jview;
 export /*bundle*/ const CompilationBackend = __compilationBackend;
 export /*bundle*/ const CompilationMobile = __compilationMobile;
+export /*bundle*/ const ConfigureGit = __configureGit;
