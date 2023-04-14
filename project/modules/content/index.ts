f7b5083ec288a7e7d832c91fc54e42cf02499d24
veD-tnayrB <Brayanmc.contacto@gmail.com>
2023-04-14 10:29:04 -0400

    Fix: Change the debug-mobile-apps variable to match with the package.json url

--- project/modules/content/index.ts
+++ project/modules/content/index.ts
@@ -1,25 +1,25 @@
 // IMPORTS
-import { Title, Info, Link } from "@bg/docs/components/html";
-import { Code } from "@bg/docs/code";
+import { Title, Info, Link } from '@bg/docs/components/html';
+import { Code } from '@bg/docs/code';
 
-import { default as __notFound } from "./mdx/not-found.mdx";
-import { default as __compilacionQch } from "./mdx/compilacion-qch/compilacion-qch.mdx";
-import { default as __plugins } from "./mdx/plugins/plugins.mdx";
-import { default as __howToUse } from "./mdx/how-to-use/how-to-use.mdx";
-import { default as __components } from "./mdx/components/components.mdx";
-import { default as __developmentStandards } from "./mdx/standards/development-process.mdx";
-import { default as _authInstalation } from "./mdx/auth/auth-instalation.mdx";
-import { default as _authSession } from "./mdx/auth/auth-session.mdx";
-import { default as _authUser } from "./mdx/auth/auth-user.mdx";
-import { default as __essentialUI } from "./mdx/ui-libraries/essential-ui.mdx";
-import { default as __beyondUI } from "./mdx/ui-libraries/beyond-ui.mdx";
-import { default as __standars } from "./mdx/standards/development-process.mdx";
-import { default as __mailer } from "./mdx/mailer/mailer.mdx";
-import { default as __jview } from "./mdx/jview/jview.mdx";
-import { default as __compilationBackend } from "./mdx/compilation-backend-external-packages/compilation-backend-external-packages.mdx";
-import { default as __compilationMobile } from "./mdx/mobile-compilations/flujocompilacionmobile.mdx";
-import { default as __configureGit } from "./mdx/configure-git/configure-git.mdx";
-import { default as __debugApps } from "./mdx/debug-mobile-apps/debug_aplicaciones_moviles.mdx";
+import { default as __notFound } from './mdx/not-found.mdx';
+import { default as __compilacionQch } from './mdx/compilacion-qch/compilacion-qch.mdx';
+import { default as __plugins } from './mdx/plugins/plugins.mdx';
+import { default as __howToUse } from './mdx/how-to-use/how-to-use.mdx';
+import { default as __components } from './mdx/components/components.mdx';
+import { default as __developmentStandards } from './mdx/standards/development-process.mdx';
+import { default as _authInstalation } from './mdx/auth/auth-instalation.mdx';
+import { default as _authSession } from './mdx/auth/auth-session.mdx';
+import { default as _authUser } from './mdx/auth/auth-user.mdx';
+import { default as __essentialUI } from './mdx/ui-libraries/essential-ui.mdx';
+import { default as __beyondUI } from './mdx/ui-libraries/beyond-ui.mdx';
+import { default as __standars } from './mdx/standards/development-process.mdx';
+import { default as __mailer } from './mdx/mailer/mailer.mdx';
+import { default as __jview } from './mdx/jview/jview.mdx';
+import { default as __compilationBackend } from './mdx/compilation-backend-external-packages/compilation-backend-external-packages.mdx';
+import { default as __compilationMobile } from './mdx/mobile-compilations/flujocompilacionmobile.mdx';
+import { default as __configureGit } from './mdx/configure-git/configure-git.mdx';
+import { default as __debugApps } from './mdx/debug-mobile-apps/debug_aplicaciones_moviles.mdx';
 
 // EXPORTS
 export /*bundle*/ const NotFound = __notFound;
@@ -39,4 +39,4 @@ export /*bundle*/ const Jview = __jview;
 export /*bundle*/ const CompilationBackend = __compilationBackend;
 export /*bundle*/ const CompilationMobile = __compilationMobile;
 export /*bundle*/ const ConfigureGit = __configureGit;
-export /*bundle*/ const DebugApps = __debugApps;
+export /*bundle*/ const DebugMobileApps = __debugApps;
