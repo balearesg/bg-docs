2a1c9d99820f67a18a6d12aa5c2b14d09fd822c5
Gabriel Castro <gaboj0531@gmail.com>
2023-09-15 18:19:05 -0400

    fix: add new array elements for menu of the sidebar

--- project/modules/layouts/main/ts/views/item.tsx
+++ project/modules/layouts/main/ts/views/item.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { URI, routing } from "@beyond-js/kernel/routing";
+import { routing } from "@beyond-js/kernel/routing";
 export function Item({ item }): JSX.Element {
+  const [label, path, subPath] = item;
+
   const navigate = (event: React.SyntheticEvent<HTMLLIElement, Event>) => {
     const { dataset }: EventTarget & HTMLLIElement = event.currentTarget;
     const url = `${dataset.path}${dataset.sub ?? ""}`
     routing.pushState(url);
   };
-  return <li onClick={navigate} data-sub={item.subPath} data-path={item.path}>{item.label}</li>;
+
+  return <li onClick={navigate} data-sub={subPath} data-path={path}>{label}</li>;
 }
